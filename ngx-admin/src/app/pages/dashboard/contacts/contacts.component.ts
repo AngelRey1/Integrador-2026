5b0f223ab67d551ae1c570a4f618abb66afee728
Angel Rey Chi Dzib <angelreychidzib5@gmail.com>
2026-03-15 11:45:39 -0600

    Chi factorizando

--- ngx-admin/src/app/pages/dashboard/contacts/contacts.component.ts
+++ ngx-admin/src/app/pages/dashboard/contacts/contacts.component.ts
@@ -1,26 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'ngx-contacts',
   styleUrls: ['./contacts.component.scss'],
   templateUrl: './contacts.component.html',
 })
-export class ContactsComponent implements OnInit {
+export class ContactsComponent {
 
   contacts: any[] = [];
   recent: any[] = [];
 
   constructor() {}
-
-  ngOnInit() {
-    // ℹ️ NOTA: Este componente originalmente usaba UserData (mock service)
-    // que devolvía usuarios ficticios (Nick, Eva, Jack, Lee, Alan, Kate)
-    // 
-    // En una aplicación real, los "contactos" en SportCONNECT son:
-    // - Para Clientes: Sus entrenadores (accesibles desde ClienteDashboard)
-    // - Para Entrenadores: Sus clientes (accesibles desde EntrenadorDashboard)
-    // 
-    // Por ahora, este componente está deshabilitado.
-    // Si necesitas mostrar contactos reales, consulta ChatFirebaseService.
-  }
 }
